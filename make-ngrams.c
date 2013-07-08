cc2763003b70d26ea0b963e52c90fc0d83d676d1
Duane Johnson <duane.johnson@gmail.com>
2013-07-07 23:41:21 -0600

    c make-ngrams WIP

--- make-ngrams.c
+++ make-ngrams.c
@@ -8,9 +8,10 @@
 #include <sys/uio.h>
 #include <unistd.h>
 
-#define BUFSIZE 65536
+#define BUFSIZE 65535
 char readbuf[BUFSIZE], writebuf[2][BUFSIZE];
-int writebuf_page = 0, writebuf_i = 0;
+int writebuf_page = 0, repeat_page = 0, next_repeat_page = 0;
+long writebuf_i = 0, repeat_i = 0, next_repeat_i = 0, repeat_target = 0;
 
 inline int letter(int c) {
   if (c >= 'A' && c <= 'Z') return c - 'A' + 'a';
@@ -38,11 +39,12 @@ inline void writechar(int fd, char c) {
 int main(int argc, char* argv[]) {
   int stdin = open("/dev/stdin", O_RDONLY);
   int stdout = open("/dev/stdout", O_WRONLY);
-  int bytes, i;
-  int repeat_i = 0, repeat_page = 0;
-  int words_so_far = 0, words_in_line = 5;
+  int bytes;
+  long i;
+  int words = 0, words_per_phrase = 5;
 
   char this_char = 0, last_char = 0;
+  char inner_this_char = 0, inner_last_char = 0;
 
   while ((bytes = read(stdin, readbuf, BUFSIZE)) != 0) {
     for (i = 0; i < bytes; i++) {
@@ -55,35 +57,46 @@ int main(int argc, char* argv[]) {
           // Word delimiter
           if (last_char != ' ') {
             // This is the first space after a word
-            words_so_far++;
-            dprintf(stdout, "[%d]", words_so_far);
-            if (words_so_far == words_in_line) {
-              words_so_far = words_in_line - 1;
+            words++;
+            if (words == words_per_phrase) {
+              words = 1;
+              writechar(stdout, ' ');
+              repeat_target = writebuf_i;
               writechar(stdout, '\n');
-              dprintf(stdout, "(%d)", repeat_i);
-              while (repeat_i != writebuf_i && repeat_page != writebuf_page) {
-                writechar(stdout, writebuf[repeat_page][repeat_i++]);
+              while (repeat_i != repeat_target) {
+                inner_this_char = writebuf[repeat_page][repeat_i++];
                 if (repeat_i == BUFSIZE) {
                   repeat_i = 0;
                   repeat_page = !repeat_page;
                 }
+                if (inner_this_char != ' ' &&
+                    inner_last_char == ' ') {
+                  words++;
+                  if (words == 2) {
+                    writechar(stdout, '>');
+                    next_repeat_i = repeat_i-1;
+                    next_repeat_page = repeat_page;
+                  }
+                }
+                writechar(stdout, inner_this_char);
+                inner_last_char = inner_this_char;
               }
+              repeat_i = next_repeat_i;
+              repeat_page = next_repeat_page;
             } else {
               writechar(stdout, ' ');
             }
           }
           break;
         default:
-          if (last_char == ' ' &&
-              words_so_far == 1) {
-            dprintf(stdout, "<|>");
+          if (last_char == ' ' && words == 1) {
+            writechar(stdout, '!');
             repeat_i = writebuf_i;
             repeat_page = writebuf_page;
           }
           // Acceptable character
           writechar(stdout, this_char);
       }
-      writeflush(stdout);
       last_char = this_char;
     }
   }

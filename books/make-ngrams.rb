387ca57b1952e99167b7d3416a8cd84b55abcd41
Duane Johnson <duane.johnson@gmail.com>
2013-07-08 14:15:07 -0600

    WIP

--- books/make-ngrams.rb
+++ books/make-ngrams.rb
@@ -1,10 +1,11 @@
 ngrams = ARGV[0].to_i
 words = []
 $stdin.each_line do |line|
-  line.gsub!(/\d+:\d+/, '')
-  line.gsub!(/^\s*\d+\s*/, '')
-  line.gsub!(/[\?\!\-\.;:",\(\)]+/, ' ')
-  words += line.downcase.split(/\s+/)
+  line.gsub!(/[^a-zA-Z \-]+/, '')
+  # line.gsub!(/\d+:\d+/, '')
+  # line.gsub!(/^\s*\d+\s*/, '')
+  # line.gsub!(/[\?\!\-\.;:",\(\)]+/, ' ')
+  words += line.downcase.split(/[\s\-]+/)
   while words.size >= ngrams
     ngram = words[0...ngrams].join(" ")
     puts ngram if ngram !~ /^\s*$/ and ngram[0] != ' '
--- make-ngrams.c
+++ make-ngrams.c
@@ -9,9 +9,10 @@
 #include <unistd.h>
 
 #define BUFSIZE 65535
+#define MAXN 20
 char readbuf[BUFSIZE], writebuf[2][BUFSIZE];
-int writebuf_page = 0, repeat_page = 0, next_repeat_page = 0;
-long writebuf_i = 0, repeat_i = 0, next_repeat_i = 0, repeat_target = 0;
+int writebuf_page = 0;
+long writebuf_i = 0;
 
 inline int letter(int c) {
   if (c >= 'A' && c <= 'Z') return c - 'A' + 'a';
@@ -41,10 +42,21 @@ int main(int argc, char* argv[]) {
   int stdout = open("/dev/stdout", O_WRONLY);
   int bytes;
   long i;
-  int words = 0, words_per_phrase = 5;
+  int words = 1, words_per_phrase = 5;
+  assert(words_per_phrase <= MAXN);
+
+  int repeat_page = 0;
+  long repeat_i = 0, repeat_target = 0;
+
+  int word_page[MAXN];
+  long word_i[MAXN];
+
+  for (i = 0; i < MAXN; i++) {
+    word_page[i] = 0;
+    word_i[i] = 0;
+  }
 
   char this_char = 0, last_char = 0;
-  char inner_this_char = 0, inner_last_char = 0;
 
   while ((bytes = read(stdin, readbuf, BUFSIZE)) != 0) {
     for (i = 0; i < bytes; i++) {
@@ -56,46 +68,48 @@ int main(int argc, char* argv[]) {
         case ' ':
           // Word delimiter
           if (last_char != ' ') {
-            // This is the first space after a word
+            writechar(stdout, ' ');
+            // if (words >= words_per_phrase) {
+            //   writechar(stdout, '\n');
+            // }
+          }
+          break;
+        default:
+
+          if (last_char == ' ') {
+            // Bookmark the start of the word
+            word_i[words % words_per_phrase] = writebuf_i;
+            word_page[words % words_per_phrase] = writebuf_page;
             words++;
-            if (words == words_per_phrase) {
-              words = 1;
-              writechar(stdout, ' ');
+
+            dprintf(stdout, "1: %ld, 2: %ld, 3: %ld, 4: %ld, 5: %ld\n",
+              word_i[0], word_i[1], word_i[2], word_i[3], word_i[4]);
+
+            // After printing the first word in a line,
+            // print the next n-1 words
+            if (words > words_per_phrase) {
+              repeat_i = word_i[words % words_per_phrase];
+              repeat_page = word_page[words % words_per_phrase];
+
+              // writechar(stdout, ' ');
               repeat_target = writebuf_i;
-              writechar(stdout, '\n');
               while (repeat_i != repeat_target) {
-                inner_this_char = writebuf[repeat_page][repeat_i++];
+                writechar(stdout, writebuf[repeat_page][repeat_i++]);
                 if (repeat_i == BUFSIZE) {
                   repeat_i = 0;
                   repeat_page = !repeat_page;
                 }
-                if (inner_this_char != ' ' &&
-                    inner_last_char == ' ') {
-                  words++;
-                  if (words == 2) {
-                    writechar(stdout, '>');
-                    next_repeat_i = repeat_i-1;
-                    next_repeat_page = repeat_page;
-                  }
-                }
-                writechar(stdout, inner_this_char);
-                inner_last_char = inner_this_char;
               }
-              repeat_i = next_repeat_i;
-              repeat_page = next_repeat_page;
+              writechar(stdout, '\n');
             } else {
-              writechar(stdout, ' ');
+              // writechar(stdout, ' ');
             }
+            // Acceptable character
+            writechar(stdout, this_char);
+          } else {
+            // Acceptable character
+            writechar(stdout, this_char);
           }
-          break;
-        default:
-          if (last_char == ' ' && words == 1) {
-            writechar(stdout, '!');
-            repeat_i = writebuf_i;
-            repeat_page = writebuf_page;
-          }
-          // Acceptable character
-          writechar(stdout, this_char);
       }
       last_char = this_char;
     }

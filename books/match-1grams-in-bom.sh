223bb375aa293636cbc7d0d6a68c75d665b199a8
Duane Johnson <duane.johnson@gmail.com>
2013-07-11 20:53:49 -0600

    Remove unused scripts

--- books/match-1grams-in-bom.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-cat $DIR/baseline2/all-1grams.txt \
-| ruby -e '$stdin.each_line{ |e| c1, c2 = e.strip.split(" "); puts "%07d %s" % [c1, c2] }' \
-| grep 0000001 \
-| awk '{print $2;}' \
-| comm -12 ../bom/1-grams.txt -

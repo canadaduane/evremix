e76a8bbdf6d0b9012525c5dc8985a22d9512148d
Duane Johnson <duane.johnson@gmail.com>
2013-07-11 20:50:55 -0600

    Make find-source helper script clearer

--- books/find-source.sh
+++ books/find-source.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-find $DIR/baseline2 -name "$1".txt.gz -exec $DIR/gunzip-egrep.sh {} "$2" \;
+find $DIR/baseline2 -name "$1".txt.gz -exec $DIR/find-source-helper.sh {} "$2" \;
